56f2eed78b84a442d3d6bc59296b58e2b8dfdd97
skoura <50641033+skoura7@users.noreply.github.com>
2021-10-18 09:37:39 +0200

    tp2

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,1 +1,31 @@
 function validation ()
+{
+
+    if (document.getElementById("nom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le nom doit contenir au moins 5 caractère !";
+       
+       
+    }  
+
+     
+     else if(document.getElementById("nom").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le prenom doit contenir au moins 5 caractère !";
+       
+    }    
+      
+     else if(document.getElementById("adresse").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "L'adresse doit contenir au moins 5 caractère !";
+        
+    }    
+     
+     else if(document.getElementById("mail").value.length < 5 ) { 
+        document.getElementById("error").innerHTML = "Le mail doit contenir au moins 5 caractère !";
+       
+    }    
+     else {
+        
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+
+     }
+
+ }
