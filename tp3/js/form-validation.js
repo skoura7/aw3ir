31626c0d883b6852af0f3c9ce0c760bdac732784
skoura <50641033+skoura7@users.noreply.github.com>
2021-11-22 20:01:17 +0100

    tp3

---
+++ tp3/js/form-validation.js
@@ -0,0 +1,35 @@
+window.onload = function () {   // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" );
+     
+     // Y mettre le code Javascript pour valider tous les champs du formulaire
+     var myModal = new bootstrap.Modal(document.getElementById('myModal'));
+
+    document.querySelector("myForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log("form submitted!");
+
+
+        if (document.getElementById("name").value.length < 5) {
+            console.log("nom error");
+            document.querySelector(".modal-title").textContent = "Erreur"
+
+            document.querySelector(".modal-body").innerHTML = "Le champs NOM doit contenir au moins 5 caractère !";  
+            myModal.show();
+
+        }
+    
+    
+        else if (document.getElementById("inputPrenom3").value.length < 5) {
+            console.log("prenom error");
+            
+            document.querySelector(".modal-title").textContent = "Erreur"
+
+            document.querySelector(".modal-body").innerHTML = "Le champs Prénom doit contenir au moins 5 caractère !";  
+            myModal.show();
+
+
+
+        }
+      });
+    };
