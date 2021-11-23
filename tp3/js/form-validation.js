2797e61cc54bbb02e9c5f86cb6cf2c4c9d7d5c93
skoura <50641033+skoura7@users.noreply.github.com>
2021-11-23 12:54:24 +0100

    tp3

--- tp3/js/form-validation.js
+++ tp3/js/form-validation.js
@@ -42,14 +42,36 @@ window.onload = function () {   // ce code est exécuter une fois que toute la p
 
 
         }
+        else if (validateDate(document.getElementById("inputDate3").value) == false) {
+            console.log("Date error");
+            document.querySelector(".modal-title").textContent = "Erreur";
+            document.querySelector(".modal-body").textContent = "VOUS VENEZ DU FUTURE !!! Veuillez saisir une date de naissance valide";
+            myModal.show();
+        }
+
         else { 
             console.log('OK');
-        document.querySelector(".modal-title").textContent = " Bienvenue " + document.getElementById("Prenom").value + document.getElementById("name").value;
-        myModal.show();}
-      })
+        document.querySelector(".modal-title").textContent = " Bienvenue " + document.getElementById("Prenom").value + document.getElementById("  name").value;
+        document.querySelector(".modal-body").innerHTML = " vous êtes nés le :" + document.getElementById("inputDate3").value + " et vous habitez à ";
+
+          myModal.show();
+        }
+      });
     }
     
     function validateEmail(email) {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
       };
+
+      function validateDate() {
+        const inputDateDeNaissance = document.getElementById('inputDate3').value;
+    
+        let dateNaissance = new Date(inputDateDeNaissance);
+        let dateNaissanceTimestamp = dateNaissance.getTime();
+    
+        let nowTimestamp = Date.now();
+    
+        return (dateNaissanceTimestamp < nowTimestamp)
+    };
+    
