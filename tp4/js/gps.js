699a062310dc806c3ffb97ebbeb26eda41c15a65
skoura <50641033+skoura7@users.noreply.github.com>
2022-01-13 16:36:44 +0100

    tp4

--- tp4/js/gps.js
+++ tp4/js/gps.js
@@ -13,6 +13,7 @@ function showPosition(position) {
   var latlon = position.coords.latitude + "," + position.coords.longitude;
   var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=14&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg`;
 
+  document.getElementById("Adresse").value = latlon;
   document.querySelector("#map").innerHTML = `<img src='${img_url}'>`;
 }
 
