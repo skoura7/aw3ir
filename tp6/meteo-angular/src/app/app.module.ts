4927a756edf7c2d9de9340557920eedb3fcc4ae9
skoura <50641033+skoura7@users.noreply.github.com>
2022-01-26 19:52:41 +0100

    tp6

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
+import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoComponent,
+    MeteoDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ tp6/meteo-angular/src/app/meteo-detail/meteo-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meteo-detail',
+  templateUrl: './meteo-detail.component.html',
+  styleUrls: ['./meteo-detail.component.css']
+})
+export class MeteoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
